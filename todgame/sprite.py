7809f4810c0fc25613d021ec9ab551ccae4a010d
Alexander Korzun <sahhash33@gmail.com>
2020-06-26 23:06:54 +0300

    [WIP] adding sprites

---
+++ todgame/sprite.py
@@ -0,0 +1,60 @@
+from todgame.drawables import Drawable, FourSideDrawable
+from todgame.side import Side
+
+from abc import abstractmethod
+
+import pygame as pg
+from svec import Vector2d
+
+
+class BasicSprite:
+    def __init__(self, coords: Vector2d):
+        self.coords = coords
+
+
+    @abstractmethod
+    def draw(self, destination: pg.Surface) -> None:
+        ...
+
+
+    def update(self, time_delta: float) -> None:
+        del self, time_delta
+        pass
+
+
+class Sprite(BasicSprite):
+    def __init__(self, coords: Vector2d, drawable: Drawable):
+        super.__init__(coords=coords)
+        self._drawable = drawable
+
+
+    def draw(self, destination: pg.Surface) -> None:
+        self._drawable.draw(coords=self.coords, destination=destination)
+
+
+class WalkingSprite(BasicSprite):
+    def __init__(
+        self,
+        coords: Vector2d,
+        four_drawable: FourSideDrawable,
+        initial_side: Side = Side.FRONT
+    ):
+        super.__init__(coords=coords)
+        self._four_drawable = four_drawable
+        self._side = initial_side
+        self._momentum = Vector2d(0, 0)
+
+
+    def draw(self, destination: pg.Surface) -> None:
+        self._four_drawable.draw(coorde=self.coords, destination=destination, side=self._side)
+
+
+    def turn(self, new_side: Side) -> None:
+        self._side = new_side
+
+
+    async def walk(self, coords_delta: Vector2d) -> Coroutine[None, Any, None]:
+        pass
+        # TODO: create a base object that manages coroutine execution and a surface to draw on
+
+
